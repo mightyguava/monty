4535d6ef58d328a74b96643b030d005b0bfa2d54
Yunchi Luo <mightyguava@gmail.com>
2018-02-03 14:17:11 -0500

    Move runner into subproc package

--- main.go
+++ main.go
@@ -1,105 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"strings"
-	"sync"
 	"syscall"
-	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/mightyguava/gomon/subproc"
 )
-
-// Runner manages running, stopping, and restarting the command we want to execute
-type Runner struct {
-	templateCmd *exec.Cmd
-	runningCmd  *exec.Cmd
-	done        chan error
-
-	mu      *sync.Mutex
-	running bool
-}
-
-// NewRunner allocates and initializes a new Runner
-func NewRunner(cmd *exec.Cmd) *Runner {
-	return &Runner{
-		templateCmd: cmd,
-		mu:          &sync.Mutex{},
-	}
-}
-
-// Start executes the command in a separate process
-func (r *Runner) Start() error {
-	c := &exec.Cmd{}
-	*c = *r.templateCmd
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	r.runningCmd = c
-	r.done = make(chan error)
-	err := c.Start()
-	if err != nil {
-		return err
-	}
-	r.running = true
-	go func() {
-		err := c.Wait()
-		log.Println("command exited")
-
-		r.mu.Lock()
-		r.running = false
-		r.mu.Unlock()
-
-		r.done <- err
-	}()
-	return nil
-}
-
-// Stop signals the command to stop with SIGINT and waits for it to stop. If it does not stop after
-// 3 seconds, it kills the command
-func (r *Runner) Stop() error {
-	c := r.runningCmd
-	if c == nil {
-		return nil
-	}
-	r.mu.Lock()
-	if !r.running {
-		r.mu.Unlock()
-		return nil
-	}
-	r.mu.Unlock()
-
-	log.Println("sending command SIGINT")
-	// Sending a signal to the negative PID signals the whole process group. Runner.Start() starts the
-	// child process within a process group.
-	if err := syscall.Kill(-c.Process.Pid, syscall.SIGINT); err != nil {
-		return err
-	}
-	select {
-	case <-r.done:
-		return nil
-	case <-time.After(3 * time.Second):
-		if err := syscall.Kill(-c.Process.Pid, syscall.SIGKILL); err != nil {
-			return errors.New("failed to kill: " + err.Error())
-		}
-		log.Println("timed out waiting for command to stop. KILLED")
-	}
-	return nil
-}
-
-// Restart calls Stop and Start in sequence
-func (r *Runner) Restart() error {
-	err := r.Stop()
-	if err != nil {
-		return err
-	}
-	return r.Start()
-}
 
 func main() {
 	log.SetFlags(0)
@@ -117,14 +29,14 @@ func main() {
 	}
 	cmdString := strings.Join(cmd[1:], " ")
 	executable := cmd[1]
-	r := NewRunner(exec.Command(executable, args...))
+	r := subproc.NewRunner(exec.Command(executable, args...))
 	w, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal("error starting file watcher: ", err)
 	}
 	dir, err := os.Getwd()
 	if err != nil {
-		log.Fatal("error getting current directly: ", err)
+		log.Fatal("error getting current directory: ", err)
 	}
 	err = w.Add(dir)
 	if err != nil {
@@ -144,7 +56,7 @@ func main() {
 
 // WatchAndRun watches for file changes and restarts the command. If it gets a SIGINT or SIGTERM, it
 // will tell the child command to exit and then exit itself.
-func WatchAndRun(r *Runner, w *fsnotify.Watcher) error {
+func WatchAndRun(r *subproc.Runner, w *fsnotify.Watcher) error {
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	for {
---
+++ subproc/runner.go
@@ -0,0 +1,97 @@
+package subproc
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"syscall"
+	"time"
+)
+
+// Runner manages running, stopping, and restarting the command we want to execute
+type Runner struct {
+	templateCmd *exec.Cmd
+	runningCmd  *exec.Cmd
+	done        chan error
+
+	mu      *sync.Mutex
+	running bool
+}
+
+// NewRunner allocates and initializes a new Runner
+func NewRunner(cmd *exec.Cmd) *Runner {
+	return &Runner{
+		templateCmd: cmd,
+		mu:          &sync.Mutex{},
+	}
+}
+
+// Start executes the command in a separate process
+func (r *Runner) Start() error {
+	c := &exec.Cmd{}
+	*c = *r.templateCmd
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	r.runningCmd = c
+	r.done = make(chan error)
+	err := c.Start()
+	if err != nil {
+		return err
+	}
+	r.running = true
+	go func() {
+		err := c.Wait()
+		log.Println("command exited")
+
+		r.mu.Lock()
+		r.running = false
+		r.mu.Unlock()
+
+		r.done <- err
+	}()
+	return nil
+}
+
+// Stop signals the command to stop with SIGINT and waits for it to stop. If it does not stop after
+// 3 seconds, it kills the command
+func (r *Runner) Stop() error {
+	c := r.runningCmd
+	if c == nil {
+		return nil
+	}
+	r.mu.Lock()
+	if !r.running {
+		r.mu.Unlock()
+		return nil
+	}
+	r.mu.Unlock()
+
+	log.Println("sending command SIGINT")
+	// Sending a signal to the negative PID signals the whole process group. Runner.Start() starts the
+	// child process within a process group.
+	if err := syscall.Kill(-c.Process.Pid, syscall.SIGINT); err != nil {
+		return err
+	}
+	select {
+	case <-r.done:
+		return nil
+	case <-time.After(3 * time.Second):
+		if err := syscall.Kill(-c.Process.Pid, syscall.SIGKILL); err != nil {
+			return errors.New("failed to kill: " + err.Error())
+		}
+		log.Println("timed out waiting for command to stop. KILLED")
+	}
+	return nil
+}
+
+// Restart calls Stop and Start in sequence
+func (r *Runner) Restart() error {
+	err := r.Stop()
+	if err != nil {
+		return err
+	}
+	return r.Start()
+}
